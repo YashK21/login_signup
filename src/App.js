c099f371df98bb87cefdc4fcdaf91a488b417c22
yashk21 <yashk8119@gmail.com>
2022-07-27 20:49:39 +0530

    login page created(without css)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Login from './comp/Login';
-import Singup from './comp/Singup';
+import "./App.css";
+import Login from "./comp/Login";
+import Singup from "./comp/Singup";
 
 function App() {
   return (
     <div>
-      <h1>From Home</h1>
-      <Login/>
-      <Singup/>
+      {/* <h1>From Singin</h1> */}
+      <Login />
+      {/* <Singup/> */}
     </div>
   );
 }
--- src/comp/Login.js
+++ src/comp/Login.js
@@ -1,9 +1,60 @@
-import react from "react"
+import react, { useState } from "react";
 const Login = () => {
-    return (
-        <div>
-            <h1>From Login</h1>
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="container">
+      {/* <h1>From Login</h1> */}
+      <div className="forms-container">
+        <div className="signin-singup">
+          <form action="" className="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <i className="fas fa-user">
+                <input
+                  type="text"
+                  value={user}
+                  onChange={(e) => {
+                    setUser(e.target.value);
+                  }}
+                  placeholder="Username"
+                />
+              </i>
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock">
+                <input
+                  type="text"
+                  value={pass}
+                  onChange={(e) => {
+                    setPass(e.target.value);
+                  }}
+                  placeholder="Password"
+                />
+              </i>
+            </div>
+            <input
+              type="submit"
+              value="Login"
+              className="btn solid"
+              onClick={handleSubmit}
+            />
+            <p className="social-text">Or Signin with social platforms</p>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-google"></i>
+              </a>
+            </div>
+          </form>
         </div>
-    )
-}
-export default Login
+      </div>
+    </div>
+  );
+};
+export default Login;
